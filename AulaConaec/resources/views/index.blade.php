54cd473c3f0d70aaba70ad79419ca0a870609b70
Edgar Garcia <gabe220788@gmail.com>
2020-06-26 18:59:10 -0500

    configuracion y rutas

---
+++ AulaConaec/resources/views/index.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+     <link rel="icon" type="image/png" sizes="223x189" href="{{asset('img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="223x189" href="{{asset('img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="223x189" href="{{asset('img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="223x189" href="{{asset('img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="223x189" href="{{asset('img/favicon.png')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="{{asset('fonts/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/fontawesome5-overrides.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Footer-Dark.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+</head>
+<body><a class="menu-toggle rounded" href="#"><i class="fa fa-bars"></i></a>
+    <nav class="navbar navbar-light navbar-expand" id="sidebar-wrapper" style="background-color: rgba(13,91,113,0.91);">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler d-none" data-target="#"></button>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav sidebar-nav" id="sidebar-nav">
+                    <li class="nav-item d-inline-flex sidebar-brand" role="presentation" style="background-color: transparent;height: 30px;"></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link" id="iniciar-sesion" data-toggle="modal" href="#login-aula">Iniciar sesión</a></li>
+                    <li class="nav-item d-inline-flex sidebar-brand" role="presentation" style="background-color: transparent;"><a class="nav-link active" href="index.html"><img class="img-fluid" src="{{asset('img/flagmx.png')}}" style="width: 55px;padding-left: 12px;"></a><a class="nav-link" href="https://translate.google.com.mx/translate?hl=es&amp;sl=es&amp;tl=en&amp;u=https%3A%2F%2Fpfbec.org%2F"><img class="img-fluid" src="{{ asset('img/flagusa.png') }}" style="width: 55px;padding-left: 12px;"></a>
+                        <a
+                            class="nav-link" href="https://translate.google.com.mx/translate?hl=es&amp;sl=es&amp;tl=pt&amp;u=https%3A%2F%2Fpfbec.org%2F"><img class="img-fluid" src="{{asset('img/flagbrazil.png')}}" style="width: 55px;padding-left: 12px;"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="inicio-fbec" style="background-color: #0D5B71;height: 193px;">
+        <div class="container text-center"><img class="img-fluid" data-aos="zoom-in" src="{{ asset('img/LOGO.png')}}" style="width: 299px;padding-top: 37px;"></div>
+    </section>
+    <div class="carousel slide" data-ride="carousel" data-interval="false" id="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active"><img class="w-100 d-block" data-aos="fade-up" src="{{ asset('img/AV-FBEC-portada.png') }}" alt="Slide Image"></div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev" style="color: rgba(118,136,161,0);"><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel" role="button" data-slide="next"></a></div>
+    </div>
+    <div class="row no-gutters" style="height: 90px;">
+        <div class="col" style="height: 25px;">
+            <h1>&nbsp;&nbsp;</h1>
+        </div>
+    </div>
+    <div class="container-fluid text-center" id="info-aulavirtual2">
+        <div class="row justify-content-center" id="texto-medida-1">
+            <div class="col-sm-8 col-md-2" style="padding-bottom: 20px;">
+                <div class="text-center" style="background-color: #ffffff;"><img class="img-fluid" data-aos="zoom-in" src="{{ asset('img/index/icono4.png')}}">
+                    <p data-aos="zoom-out" style="padding-top: 15px;">Tú decides como programar&nbsp;<br>el horario de clases<br></p>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-2" style="padding-bottom: 20px;/*box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);*/">
+                <div class="text-center" style="background-color: #ffffff;"><img class="img-fluid" data-aos="zoom-in" src="../assets/img/index/icono1.png">
+                    <p data-aos="zoom-out" style="padding-top: 15px;">Accede desde cualquier parte, tan sólo necesitas un dispositivo con conexión a Internet.<br><br></p>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-2" style="padding-bottom: 20px;">
+                <div class="text-center" style="background-color: #ffffff;"><img class="img-fluid" data-aos="zoom-in" src="../assets/img/index/icono2.png">
+                    <p data-aos="zoom-out" style="padding-top: 15px;">Docentes y Tutores Certificados SEP-CONOCER<br></p>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-2" style="padding-bottom: 20px;">
+                <div class="text-center" style="background-color: #ffffff;"><img class="img-fluid" data-aos="zoom-in" src="../assets/img/index/icono3.png">
+                    <p data-aos="zoom-out" style="padding-top: 15px;">Contenido Protegido con Derechos de Autor<br></p>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-2" style="padding-bottom: 20px;">
+                <div class="text-center" style="background-color: #ffffff;"><img class="img-fluid" data-aos="zoom-in" src="../assets/img/index/icono5.png">
+                    <p data-aos="zoom-out" style="padding-top: 15px;">Asesorías&nbsp;<br>Presenciales<br></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section id="conocenuestraAV-1">
+        <p class="text-center" style="font-size: 3vw;color: #8f9191;padding-top: 20px;padding-bottom: 20px;">CONOCE NUESTRA AULA VIRTUAL</p>
+    </section>
+    <section class="text-center" id="webconstruccion"><img class="img-fluid" data-bs-hover-animate="pulse" src="../assets/img/imagen_web_construccion.gif" style="/*max-width: 800px;*/"></section>
+    <div class="row no-gutters" style="height: 90px;">
+        <div class="col" style="height: 25px;">
+            <h1>&nbsp;&nbsp;</h1>
+        </div>
+    </div>
+    <div class="footer-dark" style="background-color: #0d5b71;padding-bottom: 20px;">
+        <footer>
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-sm-6 col-md-3 text-center item"><img class="img-fluid" src="../assets/img/fbecgrisicono.png" style="width: 252px;padding-bottom: 31px;"><center><a href="https://www.cerotec.net/estadisticas-133590/pfbec.org" title="ir a gráficas y estadísticas"><img src="https://www.cerotec.net/contador.php?t=1&s=3&i=133590" alt="https://www.pfbec.org"></a> <br><a href='https://www.pfbec.org' style='font-size:12px;' ></a><p> VISITAS </p></center></div>
+                    <div class="col-md-6 col-lg-5 col-xl-6" id="iframe">
+                        <div class="row justify-content-center"><div class="row">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d770.0608083644665!2d-99.16557035471766!3d19.281083368481628!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc3b7f54c8ab5435e!2sColegio%20Nacional%20de%20Evaluaci%C3%B3n%20y%20Certificaci%C3%B3n%20en%20Competencias%20(CONAEC)!5e0!3m2!1ses-419!2smx!4v1588816450501!5m2!1ses-419!2smx"  frameborder="0" style="border:0" allowfullscreen>
+</iframe>
+    </div></div>
+                        <div class="row text-center">
+                            <div class="col text-justify" style="color: rgb(255,255,255);">
+                                <p class="text-center" style="font-size: 14px;color: #ffffff;margin-bottom: 0px;margin-top: 10px;">Calle Niño Jesus No. 237-B&nbsp;Colonia La Joya.&nbsp;Alcaldía Tlalpan, CDMX&nbsp;C.P. 14090<br>Tel: 55 6379 9286<br></p>
+                                <a href="mailto:consultoriaserralfbec@gmail.com">
+                                    <p class="text-center" style="font-size: 14px;color: #ffffff;margin-bottom: 16px;margin-top: 5px;">Email: consultoriaserralfbec@gmail.com<br></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 text-center item" style="color: rgb(255,255,255);">
+                        <h3 style="margin-bottom: 7px;">Enlaces</h3>
+                        <ul>
+                            <li><a href="https://www.conaec.mx/">Conaec</a></li>
+                            <li><a href="https://conocer.gob.mx/">Conocer</a></li>
+                            <li><a href="https://conaeconline.yeira.training/">Conaec Online</a></li>
+                            <li><a href="#">Sistema de Información<br>y Gestión Formativas<br></a></li>
+                        </ul>
+                        <h3 style="padding-top: 17px;margin-bottom: 7px;">Legales</h3>
+                        <ul>
+                            <li><a href="https://www.pfebec.org">Aviso de Privacidad</a></li>
+                            <li><a href="https://www.pfebec.org">Términos de Uso</a></li>
+                        </ul>
+                    </div>
+                    <div class="col text-center item social" style="padding-top: 17px;"><a class="text-center" href="https://www.facebook.com/conaecmx/"><i class="icon ion-social-facebook"></i></a><a class="text-center" href="https://twitter.com/conaecmx"><i class="icon ion-social-twitter"></i></a><a class="text-center"
+                            href="https://www.instagram.com/conaec.mx/"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">COPYRIGHT © 2020&nbsp;ALVARO SERRALDE RAMOS. DERECHOS RESERVADOS.</p>
+            </div>
+        </footer>
+    </div>
+    <div class="modal fade" role="dialog" tabindex="-1" id="login-aula">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header border rounded">
+                    <div class="p-5">
+                        <div class="text-center"><img class="img-fluid" src="../assets/img/LOGO%20AULA%20VIRTUAL%202020%20PNG.png" style="margin-bottom: 20px;"></div>
+                        <form method="POST">
+                            <div class="form-group">
+                                <input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Usuario" name="usuario">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Contraseña" name="password">
+                            </div>
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox small">
+                                    <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1" style="margin-left: 0px;margin-top: 0px;"><label class="form-check-label custom-control-label" for="formCheck-1">Recordarme</label></div>
+                                </div>
+                            </div><a class="btn btn-dark btn-block text-white btn-user" role="button" href="sesiones/manejador_sesiones.php">Iniciar Sesión</a>
+                        </form>
+                        <div class="text-center"><a class="small"><br>¿Olvidaste tu contraseña?<br></a><a class="small" href="forgot-password.html">&nbsp;Recupérala aquí<br></a></div>
+                        <div class="text-center"><a class="small" href="register.html">Crear Cuenta</a></div>
+                    </div><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+            </div>
+        </div>
+    </div>
+    <script src="{{asset('jquery.min.js')}}"></script>
+    <script src="{{asset ('bootstrap.min.js')}}"></script>
+    <script src="../assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="../assets/js/menu-desplegable.js"></script>
+</body>
+
+</html>
